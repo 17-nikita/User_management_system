a111084ef1c72dc2a66ba53b2361b098ac2536d3
nikita <nikitabhandari7898@gmail.com>
2025-10-16 10:11:29 +0530

    Revert "initial commit"
    This reverts commit d12d06ba5c11015b93d0ea700f5582301adc4af0.

--- core/connections/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from core.config import settings
-from sqlalchemy.orm import declarative_base
-
-Base=declarative_base()
-
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
